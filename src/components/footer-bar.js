aca360d1f2367e690bb5b8c5f005661783167426
Marcelo Urreli <marceloricardo.urreli@adidas.com>
2023-03-30 21:32:39 +0200

    added child components and styles

---
+++ src/components/footer-bar.js
@@ -0,0 +1,9 @@
+import react from "react";
+import "../styles/footer-bar.css"
+
+const FooterBar = ({data}) => 
+    <div className="footer-bar">
+        <p>{data}</p>
+    </div>
+
+export default FooterBar
---
+++ src/components/header-bar.js
@@ -0,0 +1,9 @@
+import react from "react";
+import "../styles/header-bar.css"
+
+const HeaderBar = ({data}) => 
+    <div className="header-bar">
+        <p>{data}</p>
+    </div>
+
+export default HeaderBar
