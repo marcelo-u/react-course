596e1fc9d71737a9cb8ff50e671bd8bdb4dc25c3
Marcelo Urreli <marceloricardo.urreli@adidas.com>
2023-03-30 21:30:56 +0200

    added child components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import react from "react";
+import FooterBar from "./components/footer-bar";
+import HeaderBar from "./components/header-bar";
 
-function App () { 
-    return <div>
-        <h1>Hello from react!</h1>
-    </div>
+const data = {
+    footerMsg : "Hello from the footer",
+    headerMsg : "Hello from the header"
 }
+
+const App = () =>  
+    <div>
+        <HeaderBar data={data.footerMsg} />
+        <h1>Hello!</h1>
+        <FooterBar data={data.headerMsg}/>
+    </div>
 
 export default App;
